1a5724d1a8216015467cbfc51da265302e591516
SandroSimperl <sandrosimperl.cc@gmail.com>
2022-12-07 11:11:34 +0000

    content

--- 04_SQL_Dataimport/04_db_csv_import.sql
+++ 04_SQL_Dataimport/04_db_csv_import.sql
@@ -14,24 +14,25 @@
 /* TABLE */
     DROP TABLE IF EXISTS sandro.cats;
     CREATE TABLE IF NOT EXISTS sandro.cats(
-        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(30) NOT NULL UNIQUE DEFAULT "Cat",
+        id INT NOT NULL PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        color VARCHAR(30) NOT NULL,
         age INT(3) NOT NULL DEFAULT 0
     );
 
 /* Struktur */
     DESCRIBE sandro.cats;
 
-/* Daten */
-    INSERT INTO sandro.cats(firstName, lastName, userName, userPass) VALUES 
-        ("Beaty", "black", 5),
-        ("Romeo", "grey", 3),
-        ("Stella", "red", 7),
-        ("Gus", "striped", 4)
-    ;
-
 /* Struktur */
     DESCRIBE sandro.cats;
+
+/* Import - CSV-Datei parsen */
+    LOAD DATA LOCAL INFILE "04_SQL_Dataimport/data/cats_export.csv" INTO TABLE sandro.cats FIELDS TERMINATED BY ";" LINES TERMINATED BY "\n" IGNORE 1 ROWS;
+    -- LOAD DATA LOCAL INFILE "xx"            ladet die lokale Datei
+    -- INTO TABLE sandro.cats                 ladet die Datei in diese Tabelle
+    -- FIELDS TERMINATED BY ";"               das Trennzeichen der einzelnen Felder, in diesser Datei ;
+    -- LINES TERMINATED BY "\n"               Zeilenumbruch, also nächster Eintrag nach Zeichen (\n = newLine)
+    -- IGNORE 1 ROWS                          ignoriert angegegebene Anzahl an Zeilen, da hier die erste Zeile ja nur die Feldnamen stehen
 
 /* show Table */
     SELECT * FROM sandro.cats;
