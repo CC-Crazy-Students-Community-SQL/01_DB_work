640745072c5d4979e14b99cee4706293f42679b7
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-12-07 11:55:20 +0100

    content

--- 04_SQL_Dataimport/04_db_csv_import.sql
+++ 04_SQL_Dataimport/04_db_csv_import.sql
@@ -15,10 +15,8 @@
     DROP TABLE IF EXISTS sandro.cats;
     CREATE TABLE IF NOT EXISTS sandro.cats(
         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        userName VARCHAR(30) NOT NULL UNIQUE,
-        lastName VARCHAR(30) NOT NULL,
-        firstName VARCHAR(30) NOT NULL,
-        userPass VARCHAR(40) NOT NULL
+        name VARCHAR(30) NOT NULL UNIQUE DEFAULT "Cat",
+        age INT(3) NOT NULL DEFAULT 0
     );
 
 /* Struktur */
@@ -26,21 +24,14 @@
 
 /* Daten */
     INSERT INTO sandro.cats(firstName, lastName, userName, userPass) VALUES 
-        ("Josi", "Moe", "josi", SHA1(MD5("1234"))),
-        ("Josh", "Foe", "josh", MD5(SHA1("#7xR3"))),
-        ("John", "Doe", "john", SHA1("qwert"))    
+        ("Beaty", "black", 5),
+        ("Romeo", "grey", 3),
+        ("Stella", "red", 7),
+        ("Gus", "striped", 4)
     ;
-
-/* change Table / Structur */
-    ALTER TABLE sandro.cats ADD userPLZ INT(5) NOT NULL DEFAULT 0;
 
 /* Struktur */
     DESCRIBE sandro.cats;
-
-/* update Values */
-    UPDATE sandro.cats SET userPLZ=77880 WHERE username="josi";
-    UPDATE sandro.cats SET userPLZ=77881 WHERE id=3;
-    UPDATE sandro.cats SET userPLZ=77882 WHERE firstName="Josh";
 
 /* show Table */
     SELECT * FROM sandro.cats;
