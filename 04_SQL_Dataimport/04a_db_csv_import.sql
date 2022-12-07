4b3e073a0217db0f6a8ac7204c8eb041fc83d393
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-12-07 12:21:41 +0100

    content

--- 04_SQL_Dataimport/04a_db_csv_import.sql
+++ 04_SQL_Dataimport/04a_db_csv_import.sql
@@ -14,7 +14,7 @@
 /* TABLE */
     DROP TABLE IF EXISTS sandro.cats;
     CREATE TABLE IF NOT EXISTS sandro.cats(
-        id INT NOT NULL PRIMARY KEY,
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
         name VARCHAR(30) NOT NULL,
         color VARCHAR(30) NOT NULL,
         age INT(3) NOT NULL DEFAULT 0
@@ -27,7 +27,7 @@
     DESCRIBE sandro.cats;
 
 /* Import - CSV-Datei parsen */
-    LOAD DATA LOCAL INFILE "04_SQL_Dataimport/data/cats_export.csv" INTO TABLE sandro.cats FIELDS TERMINATED BY ";" LINES TERMINATED BY "\n" IGNORE 1 ROWS;
+    LOAD DATA LOCAL INFILE "04_SQL_Dataimport\data\cats_export_no_id.csv" INTO TABLE sandro.cats FIELDS TERMINATED BY ";" LINES TERMINATED BY "\n" IGNORE 1 ROWS;
     -- LOAD DATA LOCAL INFILE "xx"            ladet die lokale Datei
     -- INTO TABLE sandro.cats                 ladet die Datei in diese Tabelle
     -- FIELDS TERMINATED BY ";"               das Trennzeichen der einzelnen Felder, in diesser Datei ";"
