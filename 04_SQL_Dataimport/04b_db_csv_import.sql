d8a46427f7889114b26f158c5c90ba83f0bf999d
SandroSimperl <sandrosimperl.cc@gmail.com>
2022-12-08 07:59:07 +0000

    select

--- 04_SQL_Dataimport/04b_db_csv_import.sql
+++ 04_SQL_Dataimport/04b_db_csv_import.sql
@@ -1,39 +1,33 @@
-/* Import CSV ohne Index */
+/* CRUD Create | Read | Update | Delete */  
 
-/* 	 
-	import von csv Dateien
-*/
 
-/* DB */
-    DROP DATABASE IF EXISTS sandro;
-    CREATE DATABASE IF NOT EXISTS sandro;
 
-/* show DBs */
-    SHOW DATABASES;
+/* show Table */
+    SELECT * FROM sandro.cats;
 
-/* TABLE */
-    DROP TABLE IF EXISTS sandro.cats;
-    CREATE TABLE IF NOT EXISTS sandro.cats(
-        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(30) NOT NULL,
-        color VARCHAR(30) NOT NULL,
-        age INT(3) NOT NULL DEFAULT 0
-    );
+/* UPDATE --> Änderung(en) in bestehender Tab. */
+    UPDATE sandro.cats SET name = "Joe" WHERE id = 4;
+    UPDATE sandro.cats SET color = "green" WHERE id = 3;
 
-/* Struktur */
-    DESCRIBE sandro.cats;
 
-/* Struktur */
-    DESCRIBE sandro.cats;
 
-/* Import - CSV-Datei parsen */
-    LOAD DATA LOCAL INFILE "04_SQL_Dataimport/data/cats_export_no_id.csv" INTO TABLE sandro.cats FIELDS TERMINATED BY "," LINES TERMINATED BY "\n" IGNORE 1 ROWS (name, color, age);
-    -- LOAD DATA LOCAL INFILE "xx"            ladet die lokale Datei
-    -- INTO TABLE sandro.cats                 ladet die Datei in diese Tabelle
-    -- FIELDS TERMINATED BY ";"               das Trennzeichen der einzelnen Felder, in diesser Datei ";"
-    -- LINES TERMINATED BY "\n"               Zeilenumbruch, also nächster Eintrag nach Zeichen (\n = newLine)
-    -- IGNORE 1 ROWS                          ignoriert angegegebene Anzahl an Zeilen, da hier die erste Zeile ja nur die Feldnamen stehen
-    -- (naem, color, age)                     weisst die Felder zu, da ja der erste Eintrag nicht mehr die Id sondern der Name ist, somit wäre der Name sonst in ID
+/* READ - SELECT : NEUE Ergebnistabelle wird erstellt */
+    SELECT * FROM sandro.cats;                                 -- show Table
+    SELECT * FROM sandro.cats;                                 -- gesamte Tabelle
+    SELECT name FROM sandro.cats;                              -- einzelnes Feld
+    SELECT age, name FROM sandro.cats;                         -- Felder kombiniert
+    SELECT name AS "Katzen", age AS "Alter" FROM sandro.cats;  -- Felder / neue Namen
+
+/* Filtern durch WHERE */
+    SELECT name AS "Katzen", age AS "Alter" FROM sandro.cats
+        #WHERE id = 1                            -- nimm wo id 1 ist, also einzelnes Feld
+        WHERE color = "green"                    -- nimm wo color = green
+        #WHERE age >= 2                          -- nimm wo alter größer als 2
+    ;
+
+/* Delete - DS löschen | Vorsicht!! */
+    #DELETE FROM sandro.cats;                    -- Alle Daten sind weg!!
+    #DELETE FROM sandro.cats WHERE id = 2;       -- Filter wo id = 2
 
 /* show Table */
     SELECT * FROM sandro.cats;
